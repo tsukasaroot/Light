b1809170eb48aeb29fa389872b848cfbe154340f
Jordan <jordan.roucomiguez@digitalsavior.fr>
2022-12-25 11:39:55 +0100

    Update Http.php
    Im dumbb

--- core/Http.php
+++ core/Http.php
@@ -9,25 +9,20 @@ class Http
 	public static function receivedInput(): void
 	{
 		if (empty($_POST) && $json = file_get_contents('php://input')) {
-			$var = [];
-			$json = json_decode($json);
-			
-			foreach ($json as $k => $v) {
-				$var[$k] = $v;
-			}
-			$_POST = $var;
+			$json = json_decode($json, true);
+			$_POST = $json;
 		}
 	}
-	
-	public static function sendJson(mixed $data, int $code=200): void
+
+	public static function sendJson(mixed $data, int $code = 200): void
 	{
 		$data['time'] = Bench::endTime($GLOBALS['start']);
-		
+
 		header('Content-Length: ' . strlen(json_encode($data)));
 		header('Content-Type: application/json; charset=utf-8');
 
 		http_response_code($code);
-		
+
 		echo json_encode($data);
 	}
 }
